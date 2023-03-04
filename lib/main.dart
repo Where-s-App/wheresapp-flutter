4566b2582e4e5fd4f2082faf3369d01496f5dba0
lscantelbury <lhsda.snf20@uea.edu.br>
2023-03-04 15:55:44 -0400

    feat: Storing user session at cache after a successful login

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:wheresapp/providers/session_provider.dart';
+import 'package:wheresapp/views/home_page.dart';
 import 'package:wheresapp/views/login.dart';
 
 import 'api/firebase_options.dart';
+import 'models/user_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +19,8 @@ void main() async {
 
   await Hive.openBox('keys');
 
+  await Hive.openBox('session');
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -25,6 +30,14 @@ class MyApp extends HookConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final cacheUsername = Hive.box('session').get('username');
+    final cachePassword = Hive.box('session').get('password');
+
+    if (cacheUsername != null) {
+      UserModel user =
+          UserModel(username: cacheUsername, password: cachePassword);
+      ref.watch(SessionProvider.session).user = user;
+    }
     return MaterialApp(
       title: 'Wheresapp',
       theme: ThemeData(
@@ -44,7 +57,7 @@ class MyApp extends HookConsumerWidget {
                   color: Colors.black,
                   fontSize: 16,
                   fontWeight: FontWeight.w400))),
-      home: Login(),
+      home: cacheUsername != null ? const HomePage() : Login(),
     );
   }
 }
